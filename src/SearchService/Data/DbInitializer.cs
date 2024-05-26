458fdc70d8a01048dec75d842c65faed7ddee26d
Hanish-CBRE <99728826+Hanish-CBRE@users.noreply.github.com>
2024-05-26 14:30:53 +0530

    * feat: sync communication b/w services

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -19,16 +19,31 @@ public class DbInitializer
 
         var count = await DB.CountAsync<Item>();
 
-        if(count == 0)
+
+        using var scope = app.Services.CreateScope();
+        
+        var httpClient = scope.ServiceProvider.GetService<AuctionSvcHttpClient>();
+        var items = await httpClient.GetItemsForSearchDb();
+
+        Console.WriteLine(items.Count + " retured from the auction service");
+
+        if(items.Count > 0) 
         {
-            Console.WriteLine("No data - will attempt to seed");
-
-            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
-            var options = new JsonSerializerOptions{PropertyNameCaseInsensitive=true};
-
-            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
-
             await DB.SaveAsync(items);
         }
+
+
+        // Getting data from file
+        // if(count == 0)
+        // {
+        //     Console.WriteLine("No data - will attempt to seed");
+
+        //     var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+        //     var options = new JsonSerializerOptions{PropertyNameCaseInsensitive=true};
+
+        //     var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+
+        //     await DB.SaveAsync(items);
+        // }
     }
 }
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+
 
 var app = builder.Build();
 
