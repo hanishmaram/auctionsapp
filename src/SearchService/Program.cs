bc8dc628067e94b0ad2b3743b390364597fa28d5
Hanish-CBRE <99728826+Hanish-CBRE@users.noreply.github.com>
2024-05-26 13:14:47 +0530

    feat: Search Service

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,29 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+} catch(Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+
+app.Run();
